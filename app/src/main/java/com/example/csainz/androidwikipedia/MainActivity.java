9f96c7273e0ad6e24d477cfb388a30076d913333
micho10 <gatin13@yahoo.com>
2015-08-28 19:54:31 +0900

    Added Spring logo and Spring and JSON dependencies

--- app/src/main/java/com/example/csainz/androidwikipedia/MainActivity.java
+++ app/src/main/java/com/example/csainz/androidwikipedia/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.csainz.androidwikipedia;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    /**
+     * Activity lifecycle method
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
